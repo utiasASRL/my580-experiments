9045fccac756527981b05d27f2055b35468cb732
Frederike Duembgen <frederike.dumbgen@robotics.utias.utoronto.ca>
2023-05-05 15:45:32 -0400

    Change default zedcam parameters

--- workspace/scripts/run_april_left.sh
+++ workspace/scripts/run_april_left.sh
@@ -2,4 +2,4 @@ ros2 run apriltag_ros apriltag_node --ros-args \
     -r image_rect:=/zed2/zed_node/left/image_rect_color \
     -r camera_info:=/zed2/zed_node/left/camera_info \
     -r detections:=detections/left \
-    --params-file /home/ros/my580_ros2_ws/workspace/params/tags_custom48h12_left.yaml
+    --params-file /home/ros/my580_ros2_ws/workspace/params/tags_left.yaml
