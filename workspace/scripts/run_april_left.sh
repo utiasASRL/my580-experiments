211def57efae8e4ef04d8198d0b2a6f77d6a37cd
Frederike Duembgen <frederike.dumbgen@robotics.utias.utoronto.ca>
2023-05-08 21:31:39 -0400

    Change to zed2i

--- workspace/scripts/run_april_left.sh
+++ workspace/scripts/run_april_left.sh
@@ -1,5 +1,5 @@
 ros2 run apriltag_ros apriltag_node --ros-args \
-    -r image_rect:=/zed2/zed_node/left/image_rect_color \
-    -r camera_info:=/zed2/zed_node/left/camera_info \
+    -r image_rect:=/zed2i/zed_node/left/image_rect_color \
+    -r camera_info:=/zed2i/zed_node/left/camera_info \
     -r detections:=detections/left \
     --params-file /home/ros/my580_ros2_ws/workspace/params/tags_left.yaml
