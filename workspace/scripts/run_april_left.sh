2345a415722600b6ae98340fb6711d7bf6c7f196
Frederike Duembgen <frederike.dumbgen@robotics.utias.utoronto.ca>
2023-05-09 21:57:42 -0400

    Fix paths

--- workspace/scripts/run_april_left.sh
+++ workspace/scripts/run_april_left.sh
@@ -2,4 +2,4 @@ ros2 run apriltag_ros apriltag_node --ros-args \
     -r image_rect:=/zed2i/zed_node/left/image_rect_color \
     -r camera_info:=/zed2i/zed_node/left/camera_info \
     -r detections:=detections/left \
-    --params-file /home/ros/my580_ros2_ws/workspace/params/tags_left.yaml
+    --params-file /home/asrl/research/my580-experiments/workspace/params/tags_left.yaml
--- workspace/scripts/run_april_right.sh
+++ workspace/scripts/run_april_right.sh
@@ -2,4 +2,4 @@ ros2 run apriltag_ros apriltag_node --ros-args \
     -r image_rect:=/zed2i/zed_node/right/image_rect_color \
     -r camera_info:=/zed2i/zed_node/right/camera_info \
     -r detections:=detections/right \
-    --params-file /home/ros/my580_ros2_ws/workspace/params/tags_right.yaml
+    --params-file /home/asrl/research/my580-experiments/workspace/params/tags_right.yaml
